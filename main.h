f4d6611eb71f5ad900d74230a4c8e8aa11d29246
mseeam <mseeam99@gmail.com>
2022-09-11 15:16:13 -0500

    initial commit

---
+++ main.h
@@ -0,0 +1,16 @@
+/********************************************************************************
+ * File  :   main.h                                                             *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 4: Simple Int Stack                                               *
+ * This is a simple application to demonstrate Simple Int Stack in C++.         *
+ * This header structure and files for all cpp and h files.                     *
+ * This file, main.h, includes only stack.h.                                    *
+ *******************************************************************************/
+
+#ifndef STACK_MAIN_H
+#define STACK_MAIN_H
+
+#include <iostream>
+#include <stack.h>
+
+#endif //STACK_MAIN_H
