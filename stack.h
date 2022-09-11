2f25bf31c5c8555f16c13a4955fdfec8b5087b97
mseeam <mseeam99@gmail.com>
2022-09-11 15:17:18 -0500

    initial commit

---
+++ stack.h
@@ -0,0 +1,12 @@
+/********************************************************************************
+ * File  :   stack.h                                                            *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 4: Simple Int Stack                                               *
+ * This is a simple application to demonstrate Simple Int Stack in C++.         *
+ * This header structure and files for all cpp and h files.                     *                                 *
+ *******************************************************************************/
+
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+
+#endif //STACK_STACK_H
