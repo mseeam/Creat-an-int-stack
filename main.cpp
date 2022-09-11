315d5f84fe42257a6762255e9f0694595f48d218
mseeam <mseeam99@gmail.com>
2022-09-11 15:15:21 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/********************************************************************************
+ * File  :   main.cpp                                                           *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 4: Simple Int Stack                                               *
+ * This is a simple application to demonstrate Simple Int Stack in C++.         *
+ * This header structure and files for all cpp and h files.                     *
+ * This file, main.cpp, includes only main.h.                                   *
+ *******************************************************************************/
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+   std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
