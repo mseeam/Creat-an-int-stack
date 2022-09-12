90156ebcdcc381ff9e11af10db69b11ed506c2f7
mseeam <mseeam99@gmail.com>
2022-09-11 22:47:28 -0500

    Implemented class Stack,declared functions,has private and public veriables

--- stack.h
+++ stack.h
@@ -9,4 +9,26 @@
 #ifndef STACK_STACK_H
 #define STACK_STACK_H
 
+class Stack
+{
+    private:
+
+    #define MAX 10  //MAX is maximum size of stack
+    int top;
+    int stack[MAX];  //Maximum size of Stack
+
+    public:
+        
+    Stack()
+    {
+        top = -1;
+    }
+
+    //Declaring all the function
+    bool push(int value);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
 #endif //STACK_STACK_H
